a900c2f299293031e80712fd01bda43ae2eb2292
Luke Shiner <luke@lukeshiner.com>
2015-07-07 15:44:57 +0100

    Initial Commit

---
+++ linnworks_api.py
@@ -0,0 +1,53 @@
+import urllib.request
+import requests
+import json
+from pprint import pprint
+
+class LinnworksAPI:
+    def __init__(self):
+        self.username = 'stcstores@yahoo.com'
+        self.password = input('Linnworks Password: ')
+        self.get_token()
+        
+
+    def get_page(self, url, data=None):
+        if data != None:
+            data = urllib.parse.urlencode(data)
+            data = data.encode('utf-8')
+        with urllib.request.urlopen(url, data, timeout=120) as response:
+            page = str(response.read())
+            return page
+
+    def make_request(self, url, data=None):
+        request = requests.get(url, params=data)
+        print(request.url)
+        parsed_json = json.loads(request.text)
+        #pprint(parsed_json)
+        return parsed_json
+
+    def request(self, url, data=None):
+        if data == None:
+            data = {}
+        data['token'] = self.token
+        return self.make_request(url, data)
+
+    def get_token(self):
+        login_url = 'https://api.linnworks.net//api/Auth/Multilogin'
+        auth_url =  'https://api.linnworks.net//api/Auth/Authorize'
+        login_data = {'userName' : self.username, 'password' : self.password}
+        multilogin = self.make_request(login_url, login_data)
+        self.user_id = multilogin[0]['Id']
+        auth_data = login_data
+        auth_data['userId'] = self.user_id
+        authorize = self.make_request(auth_url, auth_data)
+        self.token = authorize['Token']
+        self.server = authorize['Server']
+
+    
+    
+
+api = LinnworksAPI()
+url = api.server + '/api/Inventory/GetCategories'
+data = {'token' : api.token}
+req = api.request(url)
+
