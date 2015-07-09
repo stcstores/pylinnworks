e5762d4559debd129b86d54deddbf1c1b6aef5b1
Luke Shiner <luke@lukeshiner.com>
2015-07-09 12:40:54 +0100

    Adds to_table() method to Inventory

--- inventory.py
+++ inventory.py
@@ -1,5 +1,7 @@
 from inventory_item import InventoryItem as InventoryItem
 
+from lstools . table import Table as Table
+
 class Inventory():
 
 
@@ -9,3 +11,18 @@ class Inventory():
         self.items = []
         for item in item_list:
             self.items.append(InventoryItem(item))
+
+    def to_table(self):
+        header = ['SKU', 'Title', 'Purchase Price', 'Retail Price', 'Barcode']
+        table_array = []
+        for item in self.items:
+            new_row = []
+            new_row.append(item.sku)
+            new_row.append(item.title)
+            new_row.append(item.purchase_price)
+            new_row.append(item.retail_price)
+            new_row.append(item.barcode)
+            table_array.append(new_row)
+        table = Table()
+        table.load_from_array(table_array, header)
+        return table
