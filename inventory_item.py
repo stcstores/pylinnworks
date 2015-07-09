dcd877bc9fcd29429bf1096dcc6eab58cb3ea04b
Luke Shiner <luke@lukeshiner.com>
2015-07-09 11:19:26 +0100

    Adds barcode to InventoryItem

--- inventory_item.py
+++ inventory_item.py
@@ -3,9 +3,11 @@
 class InventoryItem:
 
     def __init__(self, json):
+        self.json = json
         self.sku = json['SKU']
         self.title = json['Title']
         self.stock_id = json['Id']
         self.purchase_price = json['PurchasePrice']
-        self.RetailPrice = json['RetailPrice']
+        self.retail_price = json['RetailPrice']
+        self.barcode = json['Barcode']
         
