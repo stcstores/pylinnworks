b7364fcc26695c43b7408370cdb81a97a35328a1
Luke Shiner <luke@lukeshiner.com>
2015-08-26 12:30:26 +0100

    Adds docstring to __inti__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+"""This module contains classes allow easy interaction with the linnworks.net
+API.
+"""
+
 from . linnworks_api import LinnworksAPI
 from . inventory import Inventory
 from . inventory_item import InventoryItem
