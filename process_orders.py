9336d06e7763dafdfa36c974cc715b41d1446a60
Luke Shiner <luke@lukeshiner.com>
2015-12-23 10:30:15 +0000

    Moves hard coded login out of process_orders.py

--- process_orders.py
+++ process_orders.py
@@ -6,9 +6,8 @@ import colorama
 from . linnworks_api import LinnworksAPI as LinnworksAPI
 
 
-def process_orders():
+def process_orders(api):
     colorama.init()
-    api = LinnworksAPI(username='stcstores@yahoo.com', password='cosworth')
     clear = "\n" * 100
 
     while True:
@@ -23,7 +22,7 @@ def process_orders():
             cprint('Error: GUID for ' + order_number + ' not found', 'red')
             continue
         order_data = api.get_order_data(guid)
-        if order_data['GeneralInfo']['InvoicePrinted'] != True:
+        if order_data['GeneralInfo']['InvoicePrinted'] is not True:
             cprint('Order Not Printed!', 'red')
             response = input('Process? y / n >').lower()
             if response not in ('y', 'yes'):
@@ -35,6 +34,8 @@ def process_orders():
                 continue
         api.process_order_by_GUID(guid)
         if api.get_open_order_GUID_by_number(order_number) is not None:
-            cprint('Error: ' + order_number + ' may not be processed. GUID is ' + guid, 'red')
+            cprint(
+                'Error: ' + order_number + ' may not be processed. GUID is '
+                + guid, 'red')
         else:
             cprint(order_number + ' Processed', 'green')
