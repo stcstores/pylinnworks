c33cb03adc1387634e20f07ca70136e6fb242cc1
Luke Shiner <luke@lukeshiner.com>
2016-03-16 13:25:08 +0000

    Updates orders/__init__.py to reflect name recent name change

--- api_requests/orders/__init__.py
+++ api_requests/orders/__init__.py
@@ -1,4 +1,4 @@
-from . get_open_order_ids import GetOpenOrderIDs
+from . get_all_open_orders import GetAllOpenOrders
 from . get_open_orders import GetOpenOrders
 from . get_open_order import GetOpenOrder
 from . get_orders import GetOrders
