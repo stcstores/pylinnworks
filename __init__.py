59775652073a9d1022822c22c91730c509a036de
Luke Shiner <luke@lukeshiner.com>
2016-03-16 12:48:33 +0000

    renames requests package to api_requests to avoid clashing with requests module

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-"""This module contains classes which allow easy interaction with the
-linnworks.net API.
+"""This module contains provides classes for making requests to
+api.linnworks.net
 """
 
 from . linnworks_api import LinnworksAPI
 from . inventory import Inventory
 from . inventory_item import InventoryItem
 from . process_orders import process_orders
-from . requests import *
+from . api_requests import *
