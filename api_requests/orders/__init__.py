8e32bde8269a220bc248211507f8b75a12b005a6
Luke Shiner <luke@lukeshiner.com>
2016-03-16 13:17:22 +0000

    Adds GetOrders

--- api_requests/orders/__init__.py
+++ api_requests/orders/__init__.py
@@ -1,3 +1,4 @@
 from . get_open_order_ids import GetOpenOrderIDs
 from . get_open_orders import GetOpenOrders
 from . get_open_order import GetOpenOrder
+from . get_orders import GetOrders
---
+++ api_requests/orders/get_orders.py
@@ -0,0 +1,36 @@
+import json
+
+from .. request import Request
+from .. info import GetLocations
+
+
+class GetOrders(Request):
+    url_extension = '/api/Orders/GetOrders'
+    order_ids = []
+    location_id = ''
+    load_items = True
+    load_additional_info = True
+
+    def __init__(self, api, order_ids=None, location_id=None, load_items=None,
+                 load_additional_info=None):
+        if order_ids is not None:
+            self.order_ids = order_ids
+        if location_id is not None:
+            self.location_id = location_id
+        else:
+            self.location_id = GetLocations.default
+        if load_items is not None:
+            self.load_items = load_items
+        if load_additional_info is not None:
+            self.load_additional_info = load_additional_info
+        super().__init__(api)
+
+    def get_data(self):
+        data = {
+            'ordersIds': json.dumps(self.order_ids),
+            'fulfilmentLocationId': self.location_id,
+            'loadItems': self.load_items,
+            'loadAdditionalInfo': self.load_additional_info
+        }
+        self.data = data
+        return data
