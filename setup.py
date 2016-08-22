78f7589a04794160df1b1409a4898e4918a3824b
Luke Shiner <luke@lukeshiner.com>
2016-08-22 13:07:46 +0100

    Change setup.py to use setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pylinnworks',
@@ -9,20 +9,7 @@ setup(
     author='Luke Shiner',
     author_email='luke@lukeshiner.com',
     url='http://pylinnworks.lukeshiner.com',
-    packages=[
-        'pylinnworks',
-        'pylinnworks.api_requests',
-        'pylinnworks.api_requests.import_export',
-        'pylinnworks.api_requests.inventory',
-        'pylinnworks.api_requests.inventory.extended_properties',
-        'pylinnworks.api_requests.inventory.images',
-        'pylinnworks.api_requests.inventory.update_inventory',
-        'pylinnworks.api_requests.orders',
-        'pylinnworks.api_requests.processed_orders',
-        'pylinnworks.api_requests.settings',
-        'pylinnworks.inventory',
-        'pylinnworks.orders',
-        'pylinnworks.settings'],
+    packages=find_packages(),
     install_requires=[
         'requests',
         'simplejson'],
