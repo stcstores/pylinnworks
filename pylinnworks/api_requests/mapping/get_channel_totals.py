166843b1811a0b9de0899bacd04973d561db449c
Luke Shiner <luke@lukeshiner.com>
2016-12-15 13:36:13 +0000

    Fixes broken GetChannelTotals Request

--- pylinnworks/api_requests/mapping/get_channel_totals.py
+++ pylinnworks/api_requests/mapping/get_channel_totals.py
@@ -1,5 +1,25 @@
-from . get_channel_items import GetChannelItems
+import json
+
+from pylinnworks.api_requests.request import Request
 
 
-class GetChannelTotals(GetChannelItems):
+class GetChannelTotals(Request):
     url_extension = 'api/ChannelMapping/GetChannelTotals'
+
+    def __init__(self, api_session, channel_id, source, sub_source):
+        self.channel_id = channel_id
+        self.source = source
+        self.sub_source = sub_source
+        super().__init__(api_session)
+
+    def get_data(self):
+        data = {
+            'request': json.dumps({
+                'ChannelId': self.channel_id,
+                'Source': self.source,
+                'SubSource': self.sub_source,
+            })
+        }
+
+        self.data = data
+        return data
