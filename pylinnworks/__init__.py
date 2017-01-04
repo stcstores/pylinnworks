c60f78950bc22353b213219eaf83d08b2f5b28bc
Luke Shiner <luke@lukeshiner.com>
2017-01-04 14:08:07 +0000

    Adds main classes to __init__.py

--- pylinnworks/__init__.py
+++ pylinnworks/__init__.py
@@ -1,3 +1,8 @@
 from . pylinnworks import PyLinnworks
 from . import_export import Export
 from . processed_orders import ProcessedOrders
+from . settings import Settings
+from . linking import Linking
+from . shipping import Manifests
+from . inventory import Inventory
+from . open_orders import OpenOrders
