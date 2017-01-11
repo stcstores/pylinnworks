1c5914e17c862ac0915b5d58ddd64e964b2b0244
Luke Shiner <luke@lukeshiner.com>
2017-01-11 12:51:36 +0000

    Add kwargs to Linking to specify source and sub source to load. Also gives Linking __len__

--- pylinnworks/linking/linking.py
+++ pylinnworks/linking/linking.py
@@ -4,11 +4,23 @@ from . channel_linking import ChannelLinking
 
 
 class Linking(PyLinnworks):
-    def __init__(self, api_session=None):
+    def __init__(self, api_session=None, source=None, sub_source=None):
         self.api_session = api_session
         self.channels = Channels(self)
-        self.linking = sorted([
-            ChannelLinking(self, channel) for channel in self.channels])
+        self.source = source
+        self.sub_source = sub_source
+        self.get_linking()
+
+    def get_linking(self):
+        self.linking = []
+        for channel in self.channels:
+            if self.source is not None and channel.source != self.source:
+                continue
+            if self.sub_source is not None and \
+                    channel.sub_source != self.sub_source:
+                continue
+            self.linking.append(ChannelLinking(self, channel))
+        self.linking = sorted(self.linking)
 
     def __iter__(self):
         for channel in self.linking:
@@ -19,3 +31,6 @@ class Linking(PyLinnworks):
 
     def __repr__(self):
         return 'Linnworks Linking'
+
+    def __len(self):
+        return len(self.linking)
