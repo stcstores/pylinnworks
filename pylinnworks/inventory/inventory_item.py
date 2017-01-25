4cd9ecd9e9236b524b6a467e40384d9e76f7f921
Luke Shiner <luke@lukeshiner.com>
2017-01-25 12:13:03 +0000

    Docstring and method name improvments in InventoryItem

--- pylinnworks/inventory/inventory_item.py
+++ pylinnworks/inventory/inventory_item.py
@@ -8,11 +8,28 @@ from .. settings import Settings
 
 
 class InventoryItem:
-    """Wrapper for linnworks inventory items."""
+    """Wrapper for linnworks inventory items.
+
+    Attributes:
+        api_session (:class: `PyLinnworks`): Login information for
+            linnworks.net API.
+        stock_id (int): Stock ID (GUID) for this item.
+        images: Defaults to None. get_images method sets this to an instance
+            of InventoryItemImages providing attributes of, and methods for
+            working with this item's images.
+    """
 
     def __init__(self, api_session, stock_id):
+        """Initialise InventoryItem.
+
+        Arguments:
+            api_session (:class: `PyLinnworks`): Login information for
+                linnworks.net API.
+            stock_id (int): Stock ID (GUID) for this item.
+        """
         self.api_session = api_session
         self.stock_id = stock_id
+        self.images = None
         self.refresh()
 
     def __repr__(self):
@@ -21,16 +38,9 @@ class InventoryItem:
     def __str__(self):
         return '{}: {}'.format(self.sku, self.title)
 
-    def get_item_data(self):
-        """Download current information for item."""
-        get_item_request = api_requests.GetInventoryItemByID(
-            self.api_session, self.stock_id)
-        item_data = get_item_request.response_dict
-        return item_data
-
     def refresh(self):
         """Populate this inventory item from linnworks.net servers."""
-        item_data = self.get_item_data()
+        item_data = self._get_item_data()
         self.variation_group_name = item_data['VariationGroupName']
         self.sku = item_data['ItemNumber']
         self.title = item_data['ItemTitle']
@@ -154,7 +164,15 @@ class InventoryItem:
         self.extended_properties.append(prop)
 
     def get_images(self):
-        return InventoryItemImages(self.api_session, self.stock_id)
+        """Get images for this inventory item.
+
+        Sets attribute images to InventoryItemImages containing data about
+        this item's images.
+
+        Returns :class: `InventoryItemImages`
+        """
+        self.images = InventoryItemImages(self.api_session, self.stock_id)
+        return self.images
 
     def add_image(self, filepath):
         """Add image to item.
@@ -166,5 +184,12 @@ class InventoryItem:
             self.api_session, filepath, file_type='Image', expire_in=24)
         upload_response = upload_request.response_dict
         image_guid = upload_response[0]['FileId']
-        return api_requests.UploadImagesToInventoryItem(
+        api_requests.UploadImagesToInventoryItem(
             self.api_session, self.stock_id, [image_guid])
+        self.get_images()
+
+    def _get_item_data(self):
+        get_item_request = api_requests.GetInventoryItemByID(
+            self.api_session, self.stock_id)
+        item_data = get_item_request.response_dict
+        return item_data
