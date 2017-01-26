f5ac5c396774de12c5e8d3c44dff00180e96a5bf
Luke Shiner <luke@lukeshiner.com>
2017-01-26 11:55:25 +0000

    V1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylinnworks',
-    version='1.7',
+    version='1.8',
     description='Wrapper for linnworks.net API',
     author='Luke Shiner',
     author_email='luke@lukeshiner.com',
